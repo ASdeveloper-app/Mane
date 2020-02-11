49fef51b777aa7f73a979d9b31fc48a74013298c
arturo <arturo.araujo.alvarez@gmail.com>
2020-02-10 20:54:06 -0600

    Login y Sign up

---
+++ Organizador/config/passport.js
@@ -0,0 +1,71 @@
+const passport = require("passport");
+const User = require("../models/User");
+const facebookStrategy = require("passport-facebook").Strategy;
+const googleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+// Facebook
+passport.use(
+  new facebookStrategy({
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile)
+      //Usuarios previamente registrados
+      const user = await User.findOne({
+        facebookID: profile.id
+      })
+      if (user) {
+        return done(null, user);
+      }
+      //Usuarios nuevos
+      const newUser = await User.create({
+        facebookID: profile.id,
+        name: profile.displayName,
+        email: profile.user.email,
+        image: profile.user.picture
+      });
+      return done(null, newUser);
+    }));
+
+// Google 
+passport.use(
+  new googleStrategy({
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: "/auth/google/callback"
+    },
+    async (_, __, profile, done) => {
+      console.log("profile:", profile);
+      const user = await User.findOne({
+        googleID: profile.id
+      });
+      if (user) {
+        user.image = profile._json.picture;
+        await user.save();
+        return done(null, user);
+      }
+      const newUser = await User.create({
+        googleID: profile.id,
+        name: profile.displayName,
+        email: profile._json.email,
+        image: profile._json.picture
+      });
+      done(null, newUser);
+    }
+  )
+);
+
+module.exports = passport;
---
+++ Organizador/routes/authRoutes.js
@@ -0,0 +1,61 @@
+const {
+  Router
+} = require("express");
+const passport = require("../config/passport");
+const router = Router();
+const {
+  signupView,
+  signup,
+  loginView,
+  logout
+} = require("../controllers/authControllers.js");
+
+router.get("/signup", signupView);
+
+router.post("/signup", signup);
+
+router.get("/login", loginView);
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    failureMessage: "The input data is incorrect"
+  })
+);
+// Facebook auth Routes
+router.get("/auth/facebook", passport.authenticate("facebook", {
+  scope: ["user_photos", "email"]
+}));
+
+router.get("/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/profile",
+    failureRedirect: "/login"
+  })
+);
+
+
+// Google auth Routes
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email"
+    ]
+  })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/profile",
+    failureRedirect: "/login"
+  })
+);
+
+router.get("/logout", logout);
+
+module.exports = router;
