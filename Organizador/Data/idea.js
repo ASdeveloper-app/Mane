414631b1b35667da7251f110413c59ee3ca86a7f
Macbook pro <macbookpro@Macbooks-MacBook-Pro.local>
2020-02-13 21:44:28 -0600

    done

--- Organizador/Data/idea.js
+++ Organizador/Data/idea.js
@@ -1,5 +1,104 @@
 {
-  name: String,
-  description: String,
-  eveType:
-}
+
+{
+  "Bodas" [
+    {
+      name: "Bodas Mexico",
+      description: "Encuentra todo lo necesario en ideas para crear tu boda, crea un plan increible" ,
+      url: "https://www.bodas.com.mx/landings/home?adw=0&gs=1&idGrupo=1&cmp=ADW-Banquetes&isFuzzySearch=1&gclid=CjwKCAiA4Y7yBRB8EiwADV1hadkgO6VN2pVpRJSub8zl6c3hsGMCZA4kkiXP9NyZE-D8vVCrFl4YxxoCxF8QAvD_BwE",
+      type: String,
+    },
+    {
+      name: "La Europea México",
+      description: "Aqui esta todo lo necesario para poder tener la mejor fiesta",
+      url: "https://www.laeuropea.com.mx/eventos",
+      type: String,
+    },
+    {
+      name: "Zankyuo",
+      description: "Las bodas son todo un evento por lo que es necesario el desarrollar el mejor plan",
+      url: "https://www.zankyou.es/p/los-17-blogs-imprescindibles-de-inspiracion-para-tu-boda",
+      type: String,
+    },
+    {
+      name: "FridaEnamorada",
+      description: "Las mejores ideas para tu boda, encuentra todo lo necesario aqui.",
+       url: "http://www.fridaenamorada.com/blog",
+       type: String,
+    },
+    {
+      name: "El blog de una novia",
+      description: "Conoce todo lo mejor de las bodas y desarrolla tus propias ideas, logra el mejor evento para ti y tu pareja",
+      url: "https://www.elblogdeunanovia.com/preparativos/blogs-de-bodas-para-inspirarte/",
+      type: String,
+    }
+  ]}
+
+  {
+    "Viajes" [
+      {
+        "name": "Con Mochila",
+        "description": "La mejor información de destinos turisticos, encuentra tips de viaje",
+        url: "https://snip.ly/dxgem4#https://www.conmochila.com/",
+        type: String,
+      },
+      {
+        "name": "Lapiz nomada",
+        "description": "Creemos que todo en el mundo oculta cosas increibles, conoce nuevos lugares y aventuras a donde vayas",
+        url: "https://snip.ly/slj2fy#https://www.lapiznomada.com/",
+        type: String,
+        
+      },
+      {
+        "name": "Marcando el polo",
+        "description": "Conoce el mundo de una manera diferente, aprende que todo es posible y más cuando viajas",
+        url: "https://snip.ly/dt4wjd#https://marcandoelpolo.com/",
+        type: String,
+      },
+      {
+        "name": "Aprendiz Viajero",
+        "description": "Conoce todo el mundo con nosotros, el mundo es inmenso y esta esperando por que lo descubras",
+         url: "https://snip.ly/ye7cuz#https://www.aprendizajeviajero.com/",
+         type: String,
+      },
+      {
+        "name": "Alan por el mundo",
+        "description": "El viajar es una nueva oportunidad para encontrarte, hay lugares increibles que puedes conocer",
+         url: "https://snip.ly/19wef0#https://alanxelmundo.com/",
+         type: String,
+      }
+    ]}
+
+      {
+        "Eventos" [
+          {
+            "name": "Billeto",
+            "description": "Conoce los mejores eventos del mundo y aprende como lo hacen los mejores organizadores",
+             url: "https://billetto.co.uk/blog/?_ga=2.231950239.1405066359.1581563481-1602274378.1581563481",
+             type: String,
+          },
+          {
+            "name": "Bizbash",
+            "description": "Encuentra los mejores eventos y desarrolla tu propia idea, aprende que es lo que hace increible un evento",
+             url: "https://www.bizbash.com/?_ga=2.231950239.1405066359.1581563481-1602274378.1581563481",
+             type: String,
+          },
+          {
+            "name": "Bizabozz",
+            "description": "Tenemos los mejores tips, para que puedas desarrollar tu propio eventos en cuestión de horas",
+             url: "https://blog.bizzabo.com/?_ga=2.231950239.1405066359.1581563481-1602274378.1581563481",
+             type: String,
+          },
+          {
+            "name": "Bonjour events",
+            "description": "Es tu momento para aprender a desarrollar tus propias ideas y llevarlas acabo",
+             url: "http://bonjourevents.com/?_ga=2.231950239.1405066359.1581563481-1602274378.1581563481",
+             type: String,
+          },
+          {
+            "name": "Endles",
+            "description": "Conoce todo lo grande que puede ser un evento y desarrolla una gran idea para ser el mejor organizador",
+             url: "https://helloendless.com/blog/?_ga=2.231950239.1405066359.1581563481-1602274378.1581563481",
+             type: String,
+          }
+        ]}}
